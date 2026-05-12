e20b8587c040b290c2e5b40d1b041fce877d036c
Gleb <gleba.rafikov@yandex.ru>
2026-05-12 23:13:33 +0500

    modify download.py

--- MLOPS/lab3/download.py
+++ MLOPS/lab3/download.py
@@ -1,52 +1,31 @@
-import pandas as pd
-from sklearn.preprocessing import OrdinalEncoder
+#!/usr/bin/env python3
+"""
+Этап 1: Получение датасета
+"""
+import os
+import sys
+
+LOCAL_DATA = "data_for_model.csv"
 
 def download_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/dayekb/Basic_ML_Alg/main/cars_moldova_no_dup.csv', delimiter = ',')
-    df.to_csv("cars.csv", index = False)
-    return df
+    print("[INFO] Проверка наличия датасета...")
+    
+    if not os.path.exists(LOCAL_DATA):
+        print(f"[ERROR] Файл {LOCAL_DATA} не найден!")
+        print("[INFO] Скачивание из GitHub...")
+        import urllib.request
+        url = "https://github.com/rafGleb/ml_lab1/blob/lab_3/MLOPS/lab3/data_for_model.csv"
+        try:
+            urllib.request.urlretrieve(url, LOCAL_DATA)
+            print(f"[SUCCESS] Данные скачаны: {LOCAL_DATA}")
+        except:
+            print("[ERROR] Не удалось скачать данные")
+            sys.exit(1)
+    else:
+        print(f"[INFO] Даиасет найден: {LOCAL_DATA}")
+    
+    size = os.path.getsize(LOCAL_DATA)
+    print(f"[INFO] Размер файла: {size} байт")
 
-def clear_data(path2df):
-    df = pd.read_csv(path2df)
-    
-    cat_columns = ['Make', 'Model', 'Style', 'Fuel_type', 'Transmission']
-    num_columns = ['Year', 'Distance', 'Engine_capacity(cm3)', 'Price(euro)']
-    
-    question_dist = df[(df.Year <2021) & (df.Distance < 1100)]
-    df = df.drop(question_dist.index)
-    # Анализ и очистка данных
-    # анализ гистограмм
-    question_dist = df[(df.Distance > 1e6)]
-    df = df.drop(question_dist.index)
-    
-    # здравый смысл
-    question_engine = df[df["Engine_capacity(cm3)"] < 200]
-    df = df.drop(question_engine.index)
-    
-    # здравый смысл
-    question_engine = df[df["Engine_capacity(cm3)"] > 5000]
-    df = df.drop(question_engine.index)
-    
-    # здравый смысл
-    question_price = df[(df["Price(euro)"] < 101)]
-    df = df.drop(question_price.index)
-    
-    # анализ гистограмм
-    question_price = df[df["Price(euro)"] > 1e5]
-    df = df.drop(question_price.index)
-    
-    #анализ гистограмм
-    question_year = df[df.Year < 1971]
-    df = df.drop(question_year.index)
-    
-    df = df.reset_index(drop=True)  
-    ordinal = OrdinalEncoder()
-    ordinal.fit(df[cat_columns])
-    Ordinal_encoded = ordinal.transform(df[cat_columns])
-    df_ordinal = pd.DataFrame(Ordinal_encoded, columns=cat_columns)
-    df[cat_columns] = df_ordinal[cat_columns]
-    df.to_csv('df_clear.csv')
-    return True
-
-download_data()
-clear_data("cars.csv")
+if __name__ == "__main__":
+    download_data()
